6d1cd6d165d5fdfca9fabbfd79a394d778910e86
Avalynn <100826194+avalynndev@users.noreply.github.com>
2024-04-20 14:43:17 +0000

    added SpeedInsights

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -37,7 +37,7 @@ export default function App({ Component, pageProps }: AppProps) {
         <Component {...pageProps} />
 
         <Analytics />
-        {/** <SpeedInsights /> */}
+        <SpeedInsights />
       </NextUIProvider>
       <Footer />
     </div>
