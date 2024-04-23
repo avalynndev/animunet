918a12c632988468596d0a282b67f790b35103a8
Avalynn <100826194+avalynndev@users.noreply.github.com>
2024-04-23 11:23:26 +0530

    Update url.ts

--- config/url.ts
+++ config/url.ts
@@ -1,7 +1,7 @@
 export const url = {
-  popular: "https://api-ao.vercel.app/anime/gogoanime/popular",
-  top_airing: "https://api-ao.vercel.app/anime/gogoanime/top-airing",
-  info: "https://api-ao.vercel.app/anime/gogoanime/info/",
-  episode_link: "https://api-ao.vercel.app/anime/gogoanime/watch/",
-  search: "https://api-ao.vercel.app/anime/gogoanime/",
+  popular: "https://animetize-api.vercel.app/popular",
+  top_airing: "https://animetize-api.vercel.app/top-airing",
+  info: "https://animetize-api.vercel.app/info/",
+  episode_link: "https://animetize-api.vercel.app/watch/",
+  search: "https://animetize-api.vercel.app/",
 };
